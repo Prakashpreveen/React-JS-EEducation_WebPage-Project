906e04a418b338dd8e450bed06db08a0427e6560
Divya Prakash <prakashpreveen@gmail.com>
2024-12-31 14:22:31 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Hero />
     </main>
   );
 };

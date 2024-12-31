51d4e613700a183b62d3ca95c05d75a4c4339423
Divya Prakash <prakashpreveen@gmail.com>
2024-12-31 23:05:25 +0530

    added subscription section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Banner from "./components/Banner";
+import Subscribe from "./components/Subscribe";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Services />
       <Banner />
+      <Subscribe />
     </main>
   );
 };

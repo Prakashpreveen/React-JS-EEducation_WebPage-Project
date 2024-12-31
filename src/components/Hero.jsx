0450fd725dc52f4dcd51733e238d4d335fd42a81
Divya Prakash <prakashpreveen@gmail.com>
2024-12-31 17:57:26 +0530

    added hero section animated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { gsap } from "gsap";
 
 import Blob from "../assets/Blob.svg";
 import HeroPng from "../assets/hero.png";
+import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-title",
+      { opacity: 0, y: "100px" },
+      { opacity: 1, y: 0, duration: 1 }
+    );
+    gsap.fromTo(
+      ".hero-button",
+      { opacity: 0 },
+      { opacity: 1, duration: 1, delay: 0.5 }
+    );
+    gsap.fromTo(
+      ".hero-img",
+      { opacity: 0, x: "100px" },
+      { opacity: 1, x: 0, duration: 1, delay: 0.5 }
+    );
+  }, []);
+
   return (
     <section className="bg-light overflow-hidden relative">
       <Navbar />
@@ -13,13 +33,13 @@ const Hero = () => {
         {/* Brand Info */}
         <div className="flex flex-col justify-center py-14 md:py-0 relative z-20">
           <div className="text-center md:text-left space-y-10 lg:max-w-[400px]">
-            <h1 className="text-3xl lg:text-5xl font-bold !leading-snug">
+            <h1 className="text-3xl lg:text-5xl font-bold !leading-snug hero-title">
               Let's Learn to build a{" "}
               <span className="text-secondary">Website</span> for your business
             </h1>
 
             <div className="flex justify-center md:justify-start">
-              <button className="primary-btn flex items-center gap-2 group">
+              <button className="primary-btn flex items-center gap-2 group hero-button">
                 Get Started
                 <IoIosArrowRoundForward className="text-xl group-hover:translate-x-2 group-hover:-rotate-45 duration-300" />
               </button>
@@ -30,7 +50,7 @@ const Hero = () => {
         <div className="flex justify-center items-center">
           <img
             src={HeroPng}
-            className="w-[400px] xl:w-[600px] relative z-10 drop-shadow"
+            className="w-[400px] xl:w-[600px] relative z-10 drop-shadow hero-img"
           />
           <img
             src={Blob}
